1f5c0f09b3e699cca087253eaa833fb977bd0dc7
Pavel Skripkin <paskripkin@gmail.com>
2023-07-30 22:31:42 +0300

    asan: passed all tests

--- tasks/memory-safety/MemorySafety.cpp
+++ tasks/memory-safety/MemorySafety.cpp
@@ -3,45 +3,171 @@
 #include "llvm/Analysis/TargetTransformInfo.h"
 #include "llvm/IR/InstIterator.h"
 #include "llvm/IR/InstVisitor.h"
+#include "llvm/Transforms/Utils/ModuleUtils.h"
 #include "llvm/Passes/PassBuilder.h"
 #include "llvm/Passes/PassPlugin.h"
+#include <llvm/IR/DataLayout.h>
+#include <llvm/IR/Instruction.h>
+#include <llvm/IR/Instructions.h>
+#include <llvm/IR/PassManager.h>
+#include <llvm/IR/Value.h>
+#include <llvm/Transforms/Utils/LoopUtils.h>
+
+#include <vector>
 
 #define DEBUG_TYPE "memory-safety"
 
 using namespace llvm;
 
 namespace {
-    struct MemorySafetyPass : PassInfoMixin<MemorySafetyPass> {
-    public:
-        PreservedAnalyses run(Function &F, FunctionAnalysisManager &AM) {
-            errs() << "Running MemorySafetyPass on function " << F.getName() << "\n";
+	struct MemorySafetyPass : PassInfoMixin<MemorySafetyPass> {
+		PreservedAnalyses run(Module& F, ModuleAnalysisManager& AM)
+		{
+			IRBuilder<> builder(&(*F.begin()).front());
+			const DataLayout &data = (*F.begin()).getParent()->getDataLayout();
+			auto init = F.getOrInsertFunction("__runtime_init",
+					builder.getVoidTy()
+					);
+			auto Fun = dyn_cast<Function>(init.getCallee());
 
-            return PreservedAnalyses::all();
-        }
+			appendToGlobalCtors(F, Fun, 0);
 
-        // do not skip this pass for functions annotated with optnone
-        static bool isRequired() { return true; }
-    };
+			for (auto &i: F)
+				run_on_function(i, builder, data, F);
+
+			return PreservedAnalyses::all();
+		}
+
+		template<typename T>
+			void insert_callback_store_load(T &target, const DataLayout &data, IRBuilder<> &builder, FunctionCallee &check, bool is_store)
+			{
+				std::vector<Value *> args;
+				uint64_t size = data.getTypeStoreSize(getLoadStoreType(&target));
+
+				/* ptr */
+				args.push_back(target.getPointerOperand());
+
+				/* size */
+				args.push_back(builder.getInt32(size));
+
+				/* load */
+				args.push_back(builder.getInt8(is_store));
+
+				Value *ret = builder.CreateCall(check, args);
+				dyn_cast<Instruction>(ret)->moveBefore(&target);
+			}
+
+		void insert_redzone(AllocaInst &alloca, const DataLayout &data, IRBuilder<> &builder, FunctionCallee &poison)
+		{
+			size_t size_of_alloc = alloca.getAllocationSize(data).value();
+			std::vector<Value *> args;
+
+			// Create 32-byte poison right above allocation
+			auto high_redzone = builder.CreateAlloca(builder.getInt8Ty(), builder.getInt32(32));
+			dyn_cast<Instruction>(high_redzone)->moveBefore(&alloca);
+
+			high_redzone->setAlignment(Align(32));
+
+			// Create at least 32-byte poison right under allocation. Align to 32
+			auto low_redzone = builder.CreateAlloca(builder.getInt8Ty(), builder.getInt32(32 - size_of_alloc % 32 + 32));
+			dyn_cast<Instruction>(low_redzone)->moveAfter(&alloca);
+
+			// Unpoison allocated space
+			args.push_back(&alloca);
+			args.push_back(builder.getInt32(size_of_alloc));
+
+			Value *ret = builder.CreateCall(poison, args);
+			dyn_cast<Instruction>(ret)->moveAfter(dyn_cast<Instruction>(low_redzone));
+
+			// Set align to 32
+			alloca.setAlignment(Align(32));
+		}
+
+		void run_on_function(Function& F, IRBuilder<> &builder, const DataLayout &data, Module &mod)
+		{
+			/* Do not call on ourself */
+			if (F.getName().find("__runtime") != std::string::npos)
+				return;
+
+			errs() << "Running MemorySafetyPass on function " << F.getName() << "\n";
+
+			auto check = mod.getOrInsertFunction("__runtime_check_addr",
+					builder.getVoidTy(),
+					builder.getPtrTy(),
+					builder.getInt32Ty(),
+					builder.getInt8Ty());
+
+			auto stack_poison = mod.getOrInsertFunction("__runtime_poison",
+					builder.getVoidTy(),
+					builder.getPtrTy(),
+					builder.getInt32Ty());
+
+			auto malloc_wrapper = mod.getOrInsertFunction("__runtime_malloc",
+					builder.getPtrTy(),
+					builder.getInt32Ty());
+
+			auto free_wrapper = mod.getOrInsertFunction("__runtime_free",
+					builder.getVoidTy(),
+					builder.getPtrTy());
+
+			auto malloc = mod.getFunction("malloc");
+			if (malloc) {
+				malloc->replaceAllUsesWith(mod.getFunction("__runtime_malloc"));
+			}
+
+			auto free = mod.getFunction("free");
+			if (free) {
+				free->replaceAllUsesWith(mod.getFunction("__runtime_free"));
+			}
+
+			for (auto &bb: F) {
+				for (auto i = bb.begin(); i != bb.end(); ++i) {
+					auto &inst = *i;
+
+					if (inst.getOpcode() == Instruction::Store) {
+						auto store = dyn_cast<StoreInst>(&inst);
+						insert_callback_store_load(*store, data, builder, check, 1);
+					} else if (inst.getOpcode() == Instruction::Load) {
+						auto load = dyn_cast<LoadInst>(&inst);
+						insert_callback_store_load(*load, data, builder, check, 0);
+					} else if (inst.getOpcode() == Instruction::Alloca) {
+						auto alloca = dyn_cast<AllocaInst>(&inst);
+						insert_redzone(*alloca, data, builder, stack_poison);
+
+						// skip redzone
+						i++; i++;
+					}
+				}
+			}
+
+			errs() << F << "\n";
+		}
+
+		// do not skip this pass for functions annotated with optnone
+		static bool isRequired() { return true; }
+	};
 } // namespace
 
 /// Registration
-PassPluginLibraryInfo getPassPluginInfo() {
-    const auto callback = [](PassBuilder &PB) {
-        PB.registerPipelineParsingCallback(
-                [](StringRef Name, FunctionPassManager &FPM, auto) {
-                    if (Name == "memory-safety") {
-                        FPM.addPass(MemorySafetyPass());
-                        return true;
-                    }
-                    return false;
-                });
-    };
-    return {LLVM_PLUGIN_API_VERSION, "MemorySafetyPass",
-            LLVM_VERSION_STRING, callback};
+PassPluginLibraryInfo getPassPluginInfo()
+{
+	const auto callback = [](PassBuilder& PB) {
+		PB.registerPipelineParsingCallback(
+				[](StringRef Name, ModulePassManager& FPM, auto) {
+				if (Name == "memory-safety") {
+				FPM.addPass(MemorySafetyPass());
+				return true;
+				}
+				return false;
+				});
+	};
+	return { LLVM_PLUGIN_API_VERSION, "MemorySafetyPass",
+		LLVM_VERSION_STRING, callback };
 };
 
-extern "C" LLVM_ATTRIBUTE_WEAK PassPluginLibraryInfo llvmGetPassPluginInfo() {
-    return getPassPluginInfo();
+extern "C" LLVM_ATTRIBUTE_WEAK PassPluginLibraryInfo llvmGetPassPluginInfo()
+{
+	return getPassPluginInfo();
 }
 
 #undef DEBUG_TYPE
--- tasks/memory-safety/MemorySafetyRuntime.c
+++ tasks/memory-safety/MemorySafetyRuntime.c
@@ -3,23 +3,127 @@
 /// The code in this file is just a skeleton. You are allowed (and encouraged!)
 /// to change if it doesn't fit your needs or ideas.
 
+#include <assert.h>
+#include <errno.h>
 #include <stddef.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/mman.h>
+#include <sys/resource.h>
 
-__attribute__((used))
-extern void __runtime_init() {}
+#define SHADOW_OFFSET (1UL << 44)
+#define SHADOW_OFFSET_PTR ((unsigned char*)SHADOW_OFFSET)
+#define SHADOW_SIZE	0xfffffffffff
 
-__attribute__((used))
-extern void __runtime_cleanup() {}
+#define UL(x) ((unsigned long)(x))
+#define GENMASK(h, l) \
+	(((~UL(0)) - (UL(1) << (l)) + 1) & (~UL(0) >> (32 - 1 - (h))))
 
-__attribute__((used))
-extern void __runtime_check_addr(/* your arguments */) {}
+#define __round_mask(x, y) ((__typeof__(x))((y)-1))
+#define round_up(x, y) ((((x)-1) | __round_mask(x, y)) + 1)
 
-__attribute__((used))
-extern void *__runtime_malloc(size_t size) {
-    return NULL;
+#define POISON_VAL	0xffffffff
+
+static unsigned char* shadow_base;
+
+void __runtime_init()
+{
+	struct rlimit rlim;
+	rlim_t max_mem = 1UL << 47;
+	rlim.rlim_cur = max_mem;
+
+	setrlimit(RLIMIT_AS, &rlim);
+
+	void* shadow_base = mmap((void*)SHADOW_OFFSET, SHADOW_SIZE, PROT_READ | PROT_WRITE,
+	    MAP_PRIVATE | MAP_FIXED | MAP_ANONYMOUS | MAP_NORESERVE, -1, 0);
+
+	assert(shadow_base != MAP_FAILED);
 }
 
-__attribute__((used))
-extern void __runtime_free(void *ptr) {}
+void __runtime_poison(const void* ptr, size_t size)
+{
+	unsigned* base = ((unsigned*)(SHADOW_OFFSET_PTR + (UL(ptr) >> 3))) - 1;
+	unsigned i;
+	unsigned last_byte = 0;
+
+	assert((UL(ptr) & 7) == 0);
+
+	/* printf("size = %zu\n", size); */
+
+	/* poison upper redzone */
+	*(base++) = POISON_VAL;
+
+	/* every 32 byte gives zero */
+	for (i = size; size >= 32; size -= 32)
+		*(base++) = 0x0;
+
+	/* handle upper unaligned */
+	if (size) {
+		last_byte = ((size % 8) << ((size / 8) * 8));
+		last_byte |= GENMASK(31, ((size / 8) * 8) + 8);
+		*(base++) = last_byte;
+	}
+
+
+	/* poison upper redzone */
+	*base = POISON_VAL;
+}
+
+void __runtime_unpoison(const void* ptr)
+{
+	unsigned* base = ((unsigned*)(SHADOW_OFFSET_PTR + (UL(ptr) >> 3)));
+
+	while (*base != POISON_VAL) {
+		*base = POISON_VAL;
+		++base;
+	}
+}
+
+void __runtime_cleanup()
+{
+	munmap(SHADOW_OFFSET_PTR, SHADOW_SIZE);
+}
+
+static void report_error(const void *ptr, size_t size, int store)
+{
+	fprintf(stderr, "Illegal memory access at %p [%s]\n", ptr, store ? "store": "load");
+	exit(-1);
+}
+
+void __runtime_check_addr(const void* ptr, size_t size, int store)
+{
+	unsigned char* shadow = SHADOW_OFFSET_PTR + (UL(ptr) >> 3);
+	signed char k = *shadow;
+
+	printf("k = %hhx ptr = %p size = %zu\n", k, ptr, size);
+
+	if (size != 8) {
+		/* Casting to signed char to make sign check work */
+		if (k && ((signed char)(UL(ptr) & 7) + (signed char)size > k))
+			report_error(ptr, size, store);
+	} else {
+		if (k)
+			report_error(ptr, size, store);
+	}
+}
+
+void* __runtime_malloc(size_t size)
+{
+	void *ret;
+	int r = posix_memalign(&ret, 32, round_up(size + 32 * 2, 32));
+	if (r)
+		return NULL;
+
+	__runtime_poison(ret + 32, size);
+
+	return ret + 32;
+}
+
+void __runtime_free(void* ptr)
+{
+	__runtime_unpoison(ptr);
+}
 
 #pragma clang diagnostic pop
